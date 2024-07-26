cb702d998d4ca49c4778e09d20afb4109015b26b
Jing ZHANG <jing.zhang.ferment@gmail.com>
2024-07-26 15:32:10 +0200

    my version

--- App/Adapters/SuccessCoinAdapter.php
+++ App/Adapters/SuccessCoinAdapter.php
@@ -1,26 +1,29 @@
 <?php
 
 require_once './OC/SuccessCoin.php';
+require_once '../CryptoCurrency.php';
 
 // @TODO 1: Inclure et implémenter l'interface CryptoCurrency
-class SuccessCoinAdapter
+class SuccessCoinAdapter implements CryptoCurrency
 {
     private $coin;
 
-    public function __construct()
+    public function __construct(SuccessCoin $successCoin)
     {
         // @TODO 2 : Inclure la classe SuccessCoin
+        $this->coin = $successCoin;
     }
 
     public function getName() : string
     {
         // @TODO 3 : Récupérer le nom de la crypto monnaie à partir de la classe d'OpenClassrooms ?
-        return '[@TODO]';
+        return 'OC';
+    
     }
 
     public function getPrice() : string
     {
         // @TODO 4 : Convertir dans le bon format
-        return '[@TODO]';
+        return sprintf("%d", 200);
     }
 }
